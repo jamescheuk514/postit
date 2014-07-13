2fd19f4bd5b8d4377591aa17415cffcfdd3b829e
James Cheuk <jamescheuk514@gmail.com>
2014-07-14 01:41:30 +0800

    fixed missing partial: post_vote_area in users show

--- app/helpers/slug_helper.rb
+++ app/helpers/slug_helper.rb
@@ -1,9 +1,9 @@
 module SlugHelper
-  def generate_slug
-    self.slug = self.gsub(' ', '-').downcase
-  end
-
-  def to_param
-    self.slug
-  end
+#   def generate_slug
+#     self.slug = self.gsub(' ', '-').downcase
+#   end
+#
+#   def to_param
+#     self.slug
+#   end
 end
